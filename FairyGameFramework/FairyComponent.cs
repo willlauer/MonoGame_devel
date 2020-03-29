6dfd80a8035406538d5e95870f8e93f7b264ef3e
Will Lauer <willlauer1@gmail.com>
2020-03-28 17:37:33 -0700

    Skeleton framework

---
+++ FairyGameFramework/FairyComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyGameFramework
+{
+    /// <summary>
+    /// Component
+    /// </summary>
+    public class FairyComponent
+    {
+
+    }
+
+}
